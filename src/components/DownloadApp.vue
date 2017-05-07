0f3ed9738f88d38f7426dbdba0ca9875c2a07436
Diky Arga <dikyarga.id@gmail.com>
2017-05-07 20:52:16 +0800

    add Download app page component

---
+++ src/components/DownloadApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+   <v-col xs4>
+   </v-col>
+   <v-col md4 xs12 >
+     Apps
+
+   </v-col>
+   <v-col xs4>
+   </v-col>
+  </v-row>
+</template>
+
+<script>
+import axios from 'axios';
+let host = 'http://api.bukalelang.id/';
+export default {
+    data() {
+        return {
+          auctions: []
+        }
+    },
+    // Fetches posts when the component is created.
+    created() {
+      console.log('ini berhasil dibuat');
+
+    }
+}
+</script>
+
+<style>
+
+</style>
