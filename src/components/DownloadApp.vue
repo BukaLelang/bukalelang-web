3d15cd5c9bca20f579aeb0bb6b34f9458788322b
Diky Arga <dikyarga.id@gmail.com>
2017-05-07 21:22:01 +0800

    create download app page

--- src/components/DownloadApp.vue
+++ src/components/DownloadApp.vue
@@ -1,12 +1,26 @@
 <template>
   <v-row>
-   <v-col xs4>
+   <v-col xs2>
    </v-col>
-   <v-col md4 xs12 >
-     Apps
+   <v-col md8 xs12 >
+     <h3 class="pa-3 display-1 text-md-center text-lg-center text-xs-center">Unduh aplikasi BukaLelang</h3>
+       <p class="pa-3 text-md-center text-lg-center text-xs-center">Saat ini BukaLelang versi Web belum tersedia, <br>namun, Aplikasi BukaLelang sudah dapat di gunakan di Android dan (segera) di iOS</p>
+        <v-row>
+         <v-col xs12 md6 class="text-xs-center text-md-right" >
+            <v-btn class="pink text-xs-right text-md-right text-lg-right">Download Android
+               <v-icon right>get_app</v-icon>
+             </v-btn>
+        </v-col>
+         <v-col xs12 md6 class="text-xs-center text-md-left">
+            <v-btn class="pink" disabled>Download iOS
+               <v-icon right>get_app</v-icon>
+             </v-btn>
+        </v-col>
+      </v-row>
+
 
    </v-col>
-   <v-col xs4>
+   <v-col xs2>
    </v-col>
   </v-row>
 </template>
