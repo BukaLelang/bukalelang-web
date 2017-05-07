22ea16695f7b786970f3d83978f1d816701132a9
Diky Arga <dikyarga.id@gmail.com>
2017-05-07 20:26:14 +0800

    hide humburger menu when on md and above screen

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,29 @@
 <v-app top-toolbar>
     <header>
         <v-toolbar class="pink">
-            <v-toolbar-side-icon @click.native.stop="sidebar2 = !sidebar2"></v-toolbar-side-icon>
+            <v-toolbar-side-icon class="hidden-md-and-up" @click.native.stop="sidebar2 = !sidebar2"></v-toolbar-side-icon>
             <v-toolbar-title class="hidden-sm-and-down">BukaLelang</v-toolbar-title>
             <v-text-field prepend-icon="search" label="Search..." hide-details single-line dark></v-text-field>
             <v-toolbar-items class="hidden-sm-and-down">
-                <v-toolbar-item><router-link to="/">Home</router-link></v-toolbar-item>
-                <v-toolbar-item><router-link to="/shop">Shop</router-link></v-toolbar-item>
-                <v-menu bottom left offset-y origin="top right" transition="v-slide-y-transition">
+                <v-toolbar-item>Lelang</v-toolbar-item>
+                <v-toolbar-item>Download App</v-toolbar-item>
+                <!-- <v-menu bottom left offset-y origin="top right" transition="v-slide-y-transition">
                     <v-btn icon dark slot="activator">
                         <v-icon>more_vert</v-icon>
                     </v-btn>
                     <v-list>
                         <v-list-item >
                             <v-list-tile>
-                                <v-list-tile-title>Login</v-list-tile-title>
+                                <v-list-tile-title>Lelang</v-list-tile-title>
                             </v-list-tile>
                         </v-list-item>
                         <v-list-item >
                             <v-list-tile>
-                                <v-list-tile-title>Register</v-list-tile-title>
+                                <v-list-tile-title>Download App</v-list-tile-title>
                             </v-list-tile>
                         </v-list-item>
                     </v-list>
-                </v-menu>
+                </v-menu> -->
             </v-toolbar-items>
         </v-toolbar>
     </header>
@@ -33,12 +33,12 @@
             <v-list dense>
               <v-list-item >
                   <v-list-tile>
-                      <v-list-tile-title>Login</v-list-tile-title>
+                      <v-list-tile-title>Lelang</v-list-tile-title>
                   </v-list-tile>
               </v-list-item>
               <v-list-item >
                   <v-list-tile>
-                      <v-list-tile-title>Register</v-list-tile-title>
+                      <v-list-tile-title>Download App</v-list-tile-title>
                   </v-list-tile>
               </v-list-item>
             </v-list>
