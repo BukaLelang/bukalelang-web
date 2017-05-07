bc5bf1f6552014e5f29809f75e9c5b08813bac09
Diky Arga <dikyarga.id@gmail.com>
2017-05-07 20:52:45 +0800

    add guide and apps download button menu

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
     <header>
         <v-toolbar class="pink">
             <v-toolbar-side-icon class="hidden-md-and-up" @click.native.stop="sidebar2 = !sidebar2"></v-toolbar-side-icon>
-            <v-toolbar-title class="hidden-sm-and-down">BukaLelang</v-toolbar-title>
-            <v-text-field prepend-icon="search" label="Search..." hide-details single-line dark></v-text-field>
+            <v-toolbar-title class="hidden-sm-and-down" @click.native="gotoHomepage">BukaLelang</v-toolbar-title>
+            <v-text-field prepend-icon="search" label="Cari lelang..." hide-details single-line dark class="pr-10"></v-text-field>
             <v-toolbar-items class="hidden-sm-and-down">
-                <v-toolbar-item>Lelang</v-toolbar-item>
-                <v-toolbar-item>Download App</v-toolbar-item>
+                <v-toolbar-item @click.native="gotoHomepage">Lelang</v-toolbar-item>
+                <v-toolbar-item @click.native="gotoGuide">Panduan</v-toolbar-item>
+                <v-toolbar-item @click.native="downloadApp"><b>Download App</b></v-toolbar-item>
                 <!-- <v-menu bottom left offset-y origin="top right" transition="v-slide-y-transition">
                     <v-btn icon dark slot="activator">
                         <v-icon>more_vert</v-icon>
@@ -31,14 +32,14 @@
     <main>
         <v-sidebar class="pink darken-1" drawer v-model="sidebar2" height="100%">
             <v-list dense>
-              <v-list-item >
+              <v-list-item @click.native="gotoHomepage">
                   <v-list-tile>
-                      <v-list-tile-title>Lelang</v-list-tile-title>
+                      <v-list-tile-title >Lelang</v-list-tile-title>
                   </v-list-tile>
               </v-list-item>
-              <v-list-item >
+              <v-list-item @click.native="downloadApp">
                   <v-list-tile>
-                      <v-list-tile-title>Download App</v-list-tile-title>
+                      <v-list-tile-title >Download App</v-list-tile-title>
                   </v-list-tile>
               </v-list-item>
             </v-list>
@@ -94,6 +95,20 @@ export default {
                 }
             ]
         }
+    },
+    methods: {
+      gotoHomepage() {
+        this.$router.push('/')
+      },
+      gotoGuide() {
+        this.$router.push('/panduan')
+      },
+      gotoAuction() {
+        this.$router.push('/lelang')
+      },
+      downloadApp() {
+        this.$router.push('/apps')
+      }
     }
 }
 </script>
