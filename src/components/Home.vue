e2944343cb1535d54b55b831b9c541691773d50c
Diky Arga <dikyarga.id@gmail.com>
2017-05-07 20:15:07 +0800

    clean up / simplify auction card as fellow Material Guideline

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,24 +3,22 @@
    <v-col xs4>
    </v-col>
    <v-col md4 xs12 >
-     <v-card v-for="i in auctions" :key="i" class="ma-3 ">
-         <v-card-row class="blue-grey darken-1">
-             <v-card-title>
-                 <span class="white--text">{{i.title}}</span>
-                 <v-spacer></v-spacer>
-             </v-card-title>
-         </v-card-row>
+     <v-card v-for="i in auctions" :key="i" class="ma-3" >
          <v-card-row>
-           <img :src="i.images" style="width: 100%;" alt="">
+           <div class="thumbnail">
+             <img :src="i.images"  class="portrait" alt="Image" />
+           </div>
+           <!-- <img :src="i.images" style="width:100%;" alt=""> -->
          </v-card-row>
-         <v-card-text class="blue-grey darken-3 white--text">
-             <div >{{i.description}}</div>
+         <v-card-text class="grey lighten-4 black--text">
+           <span class="headline mb-2">{{i.title}}</span >
+             <p class="mt-2 grey-text">{{i.description}}</p>
          </v-card-text>
-         <v-card-row actions class="blue-grey darken-1 mt-0">
-             <v-btn flat class="white--text">Ikut Lelang</v-btn>
+         <v-card-row actions class="grey lighten-4 mt-0">
+             <v-btn flat class="pink--text" large>Ikut Lelang</v-btn>
              <v-spacer></v-spacer>
              <v-btn icon>
-                 <v-icon class="white--text">explore</v-icon>
+                 <v-icon class="pink--text">explore</v-icon>
              </v-btn>
          </v-card-row>
      </v-card>
@@ -42,6 +40,7 @@ export default {
     },
     // Fetches posts when the component is created.
     created() {
+      console.log('ini berhasil dibuat');
       axios.get(host + 'auctions')
       .then(response => {
         // JSON responses are automatically parsed.
@@ -56,4 +55,24 @@ export default {
 </script>
 
 <style>
+.thumbnail {
+  position: relative;
+  width: 100%;
+  height: 250px;
+  overflow: hidden;
+}
+.thumbnail img {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  height: 100%;
+  width: auto;
+  -webkit-transform: translate(-50%,-50%);
+      -ms-transform: translate(-50%,-50%);
+          transform: translate(-50%,-50%);
+}
+.thumbnail img.portrait {
+  width: 100%;
+  height: auto;
+}
 </style>
